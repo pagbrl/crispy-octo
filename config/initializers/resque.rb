6b72237e5c9a8f7ad046e5819adad41c770a14b2
gravity-zero <gravity.neo@gmail.com>
2023-05-24 16:34:29 +0200

    pu

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,3 @@
-uri = URI.parse(ENV.fetch("REDIS_URL"))
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-Resque.redis = REDIS
+require 'resque'
+
+Resque.redis = ENV.fetch("REDIS_URL")
